59b937c71ebb4e85f5011e14d2fe9b36591eb460
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-10-28 16:57:21 +0100

    feat: Create a tag component. It features zoom in on hover.

---
+++ src/components/Tag.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+type Props = {
+    tag: string,
+    classes: string,
+}
+
+const Tag = ({ tag, classes }: Props) => {
+    const [seeDetail, setSeeDetail] = useState(false);
+
+    return (
+        <>
+            < div
+                onMouseEnter={() => setSeeDetail(true)}
+                onMouseLeave={() => setSeeDetail(false)}
+                className={classes + "relative" + (seeDetail ? "z-50 overflow-visible " : "")}
+            >
+                <div
+                    onMouseLeave={() => setSeeDetail(false)}
+                    className={seeDetail ? "mx-1 my-1 border bg-slate-800 flex items-center justify-start absolute scale-150 h-16 w-16 min-w-fit px-4 py-2 rounded-md" : "mx-1"}
+                >
+                    {tag}
+                </div>
+
+            </div >
+        </>
+    )
+}
+
+export default Tag
