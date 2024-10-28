d588eb133978fa35ace8597cbd644b2b711ec285
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-10-28 16:57:00 +0100

    feat: Extract the footer for the home and game page into a component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+const Footer = () => {
+    return (
+        <footer className='py-12 px-8 text-center font-extralight text-2xl bg-slate-800'>
+            Pedro Silva @ 2024
+            <a className='mx-2 inline-block' href="https://github.com/PetrusMaximus0">
+                <i className="devicon-github-original"></i>
+            </a>
+        </footer>
+    )
+}
+
+export default Footer
