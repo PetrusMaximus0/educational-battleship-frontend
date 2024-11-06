dd6d90a7fced035091706824dd0cde8c4adebe4f
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-06 20:00:57 +0100

    feat: Add ActionBtn Component for the user to perform actions on a selected cell.

---
+++ src/components/ActionBtn.tsx
@@ -0,0 +1,17 @@
+type Props = {
+    clickHandler: () => void;
+    children: React.ReactNode;  
+}
+
+const ActionBtn = ({clickHandler, children}: Props )=>{
+    return(
+        <button
+            className={"rounded-sm bg-btnBg flex-grow px-0.5 border border-btnBorder hover:bg-btnBgHover active:bg-btnBgActive"}
+            onClick={clickHandler}
+        >
+            {children}
+        </button>
+    )
+}
+
+export default ActionBtn;
