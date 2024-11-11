0cfe984a9e8a4b2153cb59f396a753fe67f56acf
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-11 18:30:34 +0100

    refactor: Remove unused code lines.

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,4 @@
-import React from 'react'
-
-type Props = {}
-
-const ErrorPage = (props: Props) => {
+const ErrorPage = () => {
     return (
         <p className='w-fit mt-12 mx-auto text-2xl'>Error 404 - Page Not Found.</p>
     )
