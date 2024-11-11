ccdc0881e9445e50a5ad63096b73691c16c8dc7f
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-11 18:38:07 +0100

    feat: Rework the routes based on program flow and planning. Routes are simpler to understand and route to new pages created.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,30 @@
 import HomePage from './pages/HomePage'
 import GamePage from './pages/GamePage'
 import ErrorPage from './pages/ErrorPage'
+import TagSetupPage from "./pages/TagSetupPage.tsx";
+import ShipSetupPage from "./pages/ShipSetupPage.tsx";
+import ClientConnectRedirect from "./pages/ClientConnectRedirect.tsx";
 
 export const routes = [
     {
         path: "/",
-        element: <HomePage />
+        element: <HomePage />,
     },
     {
-        path: "/game/host",
-        element: <GamePage />
+        path: "/game/tag-setup",
+        element: <TagSetupPage/>        
+    },
+    {
+        path: "/game/ship-setup/:id",
+        element: <ShipSetupPage/>
+    },
+    {
+        path: "/game/client/connect/:id",
+        element: <ClientConnectRedirect/>
     },
     {
         path: "/game/join/:id",
-        element: <GamePage />
+        element: <GamePage/>
     },
     {
         path: "/*",
