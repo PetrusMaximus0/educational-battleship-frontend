58b487134a112dc9f62bde1c85f7d115972f236c
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-20 18:22:10 +0100

    test: re-add mock data for ships and boards to the mock game data module

--- src/mockGameData.ts
+++ src/mockGameData.ts
@@ -1,13 +1,13 @@
 
 // Placeholder tags
-import {CellData, CellTag, GameData} from "./types.tsx";
+import {CellData, CellState, CellTag, GameData, ShipData} from "./types.tsx";
 
 // Placeholder tags
 const inColTags: CellTag[] = ["I", "You", "He", "She", "It", "We", "They"];
 const inRowTags: CellTag[] = ["A cat", "A dog", "A parrot", "A tiger", "A mouse", "A turtle", "A bird" ];
 
 // Placeholder function to generate board.
-const generateEmptyBoard = () => {
+const generateEmptyBoard = (defaultState?: CellState ) => {
     // Construct temporary board data to render the cells.
     const tempBoardData : CellData[] = [];
     for (let i = 0; i < inRowTags.length * inColTags.length; i++) {
@@ -17,7 +17,7 @@ const generateEmptyBoard = () => {
                 x: i % inColTags.length,
                 y: Math.floor(i / inColTags.length)
             },
-            state: "hidden",
+            state: defaultState ? defaultState : "hidden",
             selected: false,
         }
         tempBoardData.push(cellData);
@@ -28,7 +28,65 @@ export const mockGameData : GameData = {
     gameId: "id",
     rowTags: inRowTags,
     colTags: inColTags,
-    playerBoardData: generateEmptyBoard(),
-    opponentBoardData: generateEmptyBoard(),
+    playerBoardData: generateEmptyBoard("miss"),
+    opponentBoardData: generateEmptyBoard("hidden"),
+    playerTurn: false,
+}
+export const mockShipData: ShipData[] = [
+    {
+        id: "1",
+        pos: {
+            x : 0,
+            y : 0,
+        },
+        numberOfSections: 2,
+        orientation : [0, 1],
+        type : "destroyer",
+        sectionStatus: ["ok", "ok"],
+    },
+    {
+        id: "2",
+        pos: {
+            x : 3,
+            y : 3,
+        },
+        numberOfSections: 2,
+        orientation : [1, 0],
+        type : "destroyer",
+        sectionStatus: ["ok", "ok"],
+    },
+    {
+        id: "3",
+        pos: {
+            x : 5,
+            y : 0,
+        },
+        numberOfSections: 2,
+        orientation : [0, 1],
+        type : "destroyer",
+        sectionStatus: ["ok", "ok"],
+    },    
+]
+
+const getBoardWithShips = () => {
+    const board = generateEmptyBoard("miss");
+    mockShipData.forEach((ship)=> {
+        for(let i = 0; i< ship.numberOfSections; i++){
+            const sectionShipCoords = [ship.pos.x + ship.orientation[0] * i, ship.pos.y + ship.orientation[1] * i];
+            console.log(sectionShipCoords);
+            const cellIndex = sectionShipCoords[0] + inColTags.length * sectionShipCoords[1];
+            console.log(cellIndex);
+            board[cellIndex].state = "ship";
+        }        
+    })
+    return board;
+}
+
+export const mockStartGameData : GameData = {
+    gameId: "id",
+    rowTags: inRowTags,
+    colTags: inColTags,
+    playerBoardData: getBoardWithShips(),
+    opponentBoardData: generateEmptyBoard("hidden"),
     playerTurn: false,
 }
