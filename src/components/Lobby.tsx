e65cf21cd531b9e57940a3912f0518da18cf848b
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2024-11-21 22:26:56 +0100

    refactor: Remove ShipSetupPage component. refactor: Extract a Lobby Component from the old ShipSetupPage. This component serves as a waiting page for when users are waiting for the other player to connect, allowing them to quickly copy the session id. It also redirects to the Home Page when the session ID is invalid.

---
+++ src/components/Lobby.tsx
@@ -0,0 +1,71 @@
+import {useEffect, useState} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+
+type props = {
+    validId : boolean,
+}
+
+const Lobby = ({validId} : props)=> {
+    // State
+    const [justCopiedId, setJustCopiedId] = useState(false);
+    const { id } = useParams();   
+    
+    //
+    const navigate = useNavigate();
+    
+    // Handlers
+    let copyHandler = 0;
+    
+    const handleCopyId = async () => {
+        if(id!==undefined) {
+            // Copy Session ID to the clipboard.
+            await navigator.clipboard.writeText(id);
+            setJustCopiedId(true);
+
+            // Set a small timer to change the text of the button.
+            clearTimeout(copyHandler);
+            copyHandler = setTimeout(() =>{
+                setJustCopiedId(false);
+            }, 5000)
+        }
+    }
+    
+    useEffect(() => {
+        let timerHandler = 0;
+        if(!validId) {
+           timerHandler = setTimeout(() =>{
+                navigate("/");
+            }, 5000)
+        }
+        return () => {
+            clearTimeout(timerHandler);
+        }
+    },[validId])
+    
+    return (
+        validId && 
+        <section className={"text-center"}>
+            <h2 className={"mb-4 text-xl"}> Waiting for player to join... </h2>
+            <div className={"text-center"}>
+                <h2 className={"mb-4 text-xl"}> Your session ID: </h2>
+                <div className={"flex gap-2 justify-center items-center"}>
+                    <p
+                        className={"my-2 flex gap-2 justify-center items-center"}
+                    >
+                        {id}
+                    </p>
+                    <button
+                        className={"hover:bg-btnBgHover active:bg-btnBgActive border px-3 py-2 rounded-sm"}
+                        onClick={handleCopyId}
+                    >
+                        {justCopiedId ? "Copied!" : "Copy ID"}
+                    </button>
+    
+                </div>
+            </div>
+        </section>
+        || <p> Invalid session Id, returning home... </p>
+    )
+}
+
+export default Lobby;
