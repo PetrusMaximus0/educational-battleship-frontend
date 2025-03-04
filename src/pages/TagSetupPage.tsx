f5e67b76f93513caaff011038ba6ab6ad88812bf
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2025-03-04 23:03:37 +0000

    refactor: Apply the BaseLayout component to the TagSetupPage Component.

--- src/pages/TagSetupPage.tsx
+++ src/pages/TagSetupPage.tsx
@@ -1,18 +1,19 @@
-import {Link, useNavigate} from "react-router-dom";
-import Footer from "../components/Footer.tsx";
+import {useNavigate} from "react-router-dom";
 import {mockGameData} from "../gameUtils/mockGameData.ts";
 import {useEffect, useState} from "react";
 import TagColumn from "../components/TagColumn.tsx";
 import {invokeHubEvent, joinHub, onHubEvent} from "../hubs/gameHub.tsx";
 import TagTemplateForm from "../components/TagTemplateForm.tsx";
 import {tagTemplate} from "../common/types.tsx";
+import BaseLayout from "../layouts/BaseLayout.tsx";
 
 const TagSetupPage = () => {
     const [rowTags, setRowTags] = useState<string[]>([]);
-    const [colTags, setColTags] = useState<string[]>([]);  
+    const [colTags, setColTags] = useState<string[]>([]);
     const [error, setError] = useState<Error|null>(null);
+
     const navigate = useNavigate();
-    
+
     const handleRemoveColTag = (tag: string) => {
         const newColTags = colTags.filter((val)=> val !== tag);
         setColTags(newColTags);
@@ -31,6 +32,8 @@ const TagSetupPage = () => {
             setRowTags([...rowTags, tag]);
         }
     }
+
+    //
     const handleSubmitTags = async () => {
         if (rowTags.length <= 0 || colTags.length <= 0) {
             setError(new Error("There must be at least one Row tag and one Column tag"))
@@ -55,10 +58,10 @@ const TagSetupPage = () => {
         if (invokeError) {
             setError(invokeError);
         }
-
     }
-    
+
     useEffect(()=>{
+        // Loads defaults
         setRowTags(mockGameData.rowTags);
         setColTags(mockGameData.colTags);
     },[])
@@ -69,54 +72,38 @@ const TagSetupPage = () => {
     }
 
     return (
-        <div className='grid grid-rows-[auto_1fr_auto] gap-y-10 h-full min-h-screen bg-BgB text-white'>
-            <header className='bg-BgA py-8 px-8'>
-                <div className='flex flex-col items-center justify-center gap-6'>
-                    <h1 className='text-4xl'> Game Setup </h1>
-                    <Link
-                        className='hover:bg-btnBgHover border border-Text px-4 py-3 bg-btnBg active:bg-btnBgActive rounded-md'
-                        to="/"
-                    >
-                        Return Home
-                    </Link>
+        <BaseLayout headerTitle={"Tag Setup"}>
+            <section className='flex flex-col justify-start gap-6' >
+                <h2 className={"text-4xl text-center"}> Current Tag Selection </h2>
+                <div className={"bg-BgA grid grid-cols-2 justify-center items-start gap-6 border w-fit mx-auto px-4 py-4 rounded-md"}>
+                    <div className={"col-span-2"}> <TagTemplateForm handleSubmit={handleTemplateSelectSubmit}/> </div>
+                    <TagColumn
+                        title={"Row Tags"}
+                        tags={rowTags}
+                        handleAddTag={handleAddRowTag}
+                        handleRemoveTag={handleRemoveRowTag}
+                    />
+                    <TagColumn
+                        title={"Col Tags"}
+                        tags={colTags}
+                        handleAddTag={handleAddColTag}
+                        handleRemoveTag={handleRemoveColTag}
+                    />
                 </div>
-            </header>
-            <main className={"flex gap-3 justify-start items-center w-fit mx-auto"}>
-                <section className='flex flex-col justify-start gap-6' >
-                    <h2 className={"text-4xl text-center"}> Current Tag Selection </h2>
-                    <div className={"bg-BgA grid grid-cols-2 justify-center items-start gap-6 border w-fit mx-auto px-4 py-4 rounded-md"}>
-                        <aside className={"col-span-2"}>
-                            <TagTemplateForm handleSubmit={handleTemplateSelectSubmit}/>
-                        </aside>
-                        <TagColumn
-                            title={"Row Tags"} 
-                            tags={rowTags} 
-                            handleAddTag={handleAddRowTag} 
-                            handleRemoveTag={handleRemoveRowTag} 
-                        />    
-                        <TagColumn 
-                            title={"Col Tags"} 
-                            tags={colTags} 
-                            handleAddTag={handleAddColTag} 
-                            handleRemoveTag={handleRemoveColTag} 
-                        />
-                    </div>
-                    {error && 
-                        <p className={"text-red-600 font-semibold mx-auto text-center text-xl"}> 
-                            {error.message} 
-                        </p> 
-                    }
-                    <button
-                        type='button'    
-                        className={"text-center border py-2 px-3 mx-auto rounded-md hover:bg-btnBgHover active:bg-btnBgActive"}
-                        onClick={handleSubmitTags}
-                    > 
-                        Begin Game
-                    </button>
-                </section>
-            </main>
-            <Footer/>
-        </div>
+                {error &&
+                    <p className={"text-red-600 font-semibold mx-auto text-center text-xl"}>
+                        {error.message}
+                    </p>
+                }
+                <button
+                    type='button'
+                    className={"text-center border py-2 px-3 mx-auto rounded-md hover:bg-btnBgHover active:bg-btnBgActive"}
+                    onClick={handleSubmitTags}
+                >
+                    Begin Game
+                </button>
+            </section>
+        </BaseLayout>
     )
 }
 
