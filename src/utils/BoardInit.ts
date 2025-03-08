10ac2c180947a79cabc2f7a7e6a1c95372d7e962
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2025-03-08 01:17:23 +0000

    feat: Export a previously private method in the BoardInit module. Make the method pure.

--- src/utils/BoardInit.ts
+++ src/utils/BoardInit.ts
@@ -3,10 +3,10 @@ import {ECellState} from "../enums/Enums.ts";
 import {templates} from "../data/tagTemplates.ts";
 
 // Placeholder function to generate board.
-const generateEmptyBoard = (defaultState?: ECellState ) => {
+export const generateEmptyBoard = (rowTags: string[], colTags: string[], defaultState?: ECellState ) => {
     // Construct temporary board data to render the cells.
     const tempBoardData : CellData[] = [];
-    for (let i = 0; i < templates[0].rows.length * templates[0].cols.length; i++) {
+    for (let i = 0; i < rowTags.length * colTags.length; i++) {
         const cellData: CellData = {
             index: i,
             pos: {
@@ -26,7 +26,7 @@ export const emptyBoards : GameData = {
     gameId: "id",
     rowTags : templates[0].rows,
     colTags : templates[0].cols,
-    playerBoardData : generateEmptyBoard(ECellState.miss),
-    opponentBoardData : generateEmptyBoard(ECellState.hidden),
+    playerBoardData : generateEmptyBoard(templates[0].rows, templates[0].cols, ECellState.miss),
+    opponentBoardData : generateEmptyBoard(templates[0].rows, templates[0].cols, ECellState.hidden),
     playerTurn : false,
 }
