baf6f708a0ee2882b636102efe12155456100a2c
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2025-03-08 01:14:31 +0000

    feat: Add local game ship fleet template.

---
+++ src/data/shipTemplates.ts
@@ -0,0 +1,49 @@
+import { EShipType } from "../enums/Enums.ts";
+import {ShipData} from "../types/types.tsx";
+
+let id = 0;
+const getId = ()=> `${++id}`;
+
+export const shipTemplates : ShipData[] =
+    [
+        {
+            id: getId(),
+            numberOfSections : 2,
+            orientation : [1, 0],
+            pos : {x:0, y:0},
+            type : EShipType.destroyer,
+            sectionStatus : ["ok", "ok"]
+        },
+        {
+            id: getId(),
+            numberOfSections : 3,
+            orientation : [1, 0],
+            pos : {x:0, y:0},
+            type : EShipType.submarine,
+            sectionStatus : ["ok", "ok", "ok"]
+        },
+        {
+            id: getId(),
+            numberOfSections : 3,
+            orientation : [1, 0],
+            pos : {x:0, y:0},
+            type : EShipType.frigate,
+            sectionStatus : ["ok", "ok", "ok"]
+        },
+        {
+            id: getId(),
+            numberOfSections : 4,
+            orientation : [1, 0],
+            pos : {x:0, y:0},
+            type : EShipType.battleship,
+            sectionStatus : ["ok", "ok", "ok", "ok"]
+        },
+        {
+            id: getId(),
+            numberOfSections : 5,
+            orientation : [1, 0],
+            pos : {x:0, y:0},
+            type : EShipType.carrier,
+            sectionStatus : ["ok", "ok", "ok", "ok", "ok"]
+        },
+    ];
