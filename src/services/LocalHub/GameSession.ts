e75b22456db609aa546d43a6b8af24ff21cbd7db
PetrusMaximus0 <45740112+PetrusMaximus0@users.noreply.github.com>
2025-03-08 01:27:44 +0000

    feat-wip: Add methods and properties to the GameSession class.

--- src/services/LocalHub/GameSession.ts
+++ src/services/LocalHub/GameSession.ts
@@ -1,8 +1,23 @@
-// TODO: Game session class. Holds game board data, player data, etc.
+/** Game session class. Holds game board data, player data, etc.
+ * TODO: Game Logic.
+ * */
+
+import {EClientState, EGameState} from "../../enums/Enums.ts";
+import {CellData} from "../../types/types.tsx";
+import {generateEmptyBoard} from "../../utils/BoardInit.ts";
+
 export class GameSession {
     constructor(colTags: string[], rowTags: string[]    ) {
-        console.log("Starting local session!");
-        console.log(colTags, rowTags);
+        this.rowTags = rowTags;
+        this.colTags = colTags;
+        this.playerBoard = generateEmptyBoard(this.rowTags, this.colTags);
+        console.log("Instantiated a Game Session");
     }
-}
 
+    gameState: EGameState = EGameState.FleetSetup;
+    clientState: EClientState = EClientState.FleetSetup;
+    rowTags: string[];
+    colTags: string[];
+    playerBoard: CellData[];
+    computerBoard : CellData[];
+}
